ec9c0ea6c80f674edeb8de07979b011188151965
Sharon Cichelli <sharon@invisible-city.com>
2013-08-03 16:30:05 -0500

    drawing an 8x8 board with clicky pieces

---
+++ GameBoard/GameBoard.UI/Controllers/BoardController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using GameBoard.UI.Models;
+
+namespace GameBoard.UI.Controllers
+{
+    public class BoardController : Controller
+    {
+        public ActionResult Index()
+        {
+            var board = new Board {Game = new Game {Name = "Eight by Eight"}, Width = 8, Height = 8};
+            return View(board);
+        }
+    }
+}
---
+++ GameBoard/GameBoard.UI/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace GameBoard.UI.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+    }
+
+    public class Board
+    {
+        public Game Game { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
